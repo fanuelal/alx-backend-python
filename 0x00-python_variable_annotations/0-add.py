6c987c6abfe895f978eebff074240b44e95e19bf
fanuelal <fanuelalmawstop@gmail.com>
2022-04-14 23:19:57 -0700

    adding function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" module that returns the sum """
+
+
+def add(a: float, b: float) -> float:
+    """" adding function """
+    return a+b                                
