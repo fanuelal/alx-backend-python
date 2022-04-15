61ff41cd3f0f2e27a2ae2d652b1c8a059ce450b5
fanuelal <fanuelalmawstop@gmail.com>
2022-04-15 00:02:41 -0700

    sum of list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""Module sum float"""
+
+
+def sum_list(input_list: float = []) -> float:
+    """ sum of the list float"""
+    sum = 0
+    for i in input_list:
+        sum += i
+    return sum
