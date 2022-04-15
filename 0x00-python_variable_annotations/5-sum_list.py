7daeb44f3a464ce95060cdd2cc1e54bd3b7d42f8
fanuelal <fanuelalmawstop@gmail.com>
2022-04-15 00:32:26 -0700

    fixed for typing list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 """Module sum float"""
+from typing import List
 
 
-def sum_list(input_list: float = []) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ sum of the list float"""
     sum = 0
     for i in input_list:
