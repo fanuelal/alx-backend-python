11f3c9ad00cbd0151757ab2f88869330ecc5ceeb
fanuelal <fanuelalmawstop@gmail.com>
2022-04-19 11:19:58 -0700

    random value

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" module of async """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 0) -> float:
+    """
+    returns the random value generated for asyncio
+    """
+    randValue: float = random.uniform(0, max_delay)
+    await asyncio.sleep(randValue)
+    return randValue
+    
