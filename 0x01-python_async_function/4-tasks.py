c1a0c356e2e988edd6b432df1acfd0b1566374c3
fanuelal <fanuelalmawstop@gmail.com>
2022-04-19 13:14:03 -0700

    tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""module of multiple coroutines """
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+       Args:
+            max_delay: max sleep
+            n: list size
+       Return:
+           float time rand
+    """
+    vals: List[asyncio.Task] = []
+    rand: List[float] = []
+    for _ in range(n):
+        vals.append(wait_random(max_delay))
+
+    for val in asyncio.as_completed((vals)):
+        delay = await val
+        rand.append(delay)
+
+    return rand
